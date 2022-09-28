7236e686e3fae7a8b0727251462c0b198b405079
kelly96e <kellygashi00@gmail.com>
2022-09-28 15:48:17 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,106 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+
+        /* UPPGIFT 1 */
+        /*
+        int tal = 10;
+        int tal1 = 15;
+
+        int answer = tal * tal1;
+
+        System.out.println(answer);
+        */
+
+        /* UPPGIFT 2*/
+        /*
+        String namn = "Kelly";
+        String efternamn = "Gashi";
+
+        String fullnamn = namn  +  efternamn;
+        System.out.println(fullnamn);
+        */
+
+        /* UPPGIFT 3 */
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your age?");
+        int age = scanner.nextInt();
+
+        System.out.println("What is your name?");
+        String name = scanner.next();
+
+
+        System.out.println("What is your favorite activity?");
+        String activity = scanner.next();
+
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(activity);
+        */
+
+        /* UPPGIFT 4 */
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first integer:");
+        int firstint = scanner.nextInt();
+
+        System.out.println("Enter second integer:");
+        int secondint = scanner.nextInt();
+
+        if (firstint > secondint){
+            System.out.println(firstint + " was the bigger integer");
+        }
+        else {
+            System.out.println(secondint + " was the bigger integer");
+        }
+        */
+
+        /* UPPGIFT 5 */
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first integer:");
+        int firstint = scanner.nextInt();
+
+        System.out.println("Enter second integer:");
+        int secondint = scanner.nextInt();
+
+        if (firstint < secondint){
+            System.out.println(firstint + " was the less integer");
+        }
+        else {
+            System.out.println(secondint + " was the less integer");
+        }
+        */
+
+        /* UPPGIFT 6 */
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many kilos apple do you want to buy?");
+        int apple = scanner.nextInt();
+        int kilosApple = apple * 7;
+
+        System.out.println("How many avocados do you want to buy?");
+        int avocado = scanner.nextInt();
+        int avocados = avocado * 15;
+
+        int answer = kilosApple + avocados;
+        System.out.println("The total amount is " + answer + ":-" );
+        */
+
+        /* UPPGIFT 7*/
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert integer please");
+
+        System.out.println("Start: ");
+        int Start = scanner.nextInt();
+
+        for (int i =Start; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
+        */
     }
 }
